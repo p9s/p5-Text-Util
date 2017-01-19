047746cf19f7f00a9fe8140cd4e23dd291577559
mccheung <mc_cheung@126.com>
2017-01-19 17:41:42 +0800

    done for module

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::Util - It's new $module
+
+# SYNOPSIS
+
+    use Text::Util;
+
+# DESCRIPTION
+
+Text::Util is ...
+
+# LICENSE
+
+Copyright (C) mccheung.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mccheung &lt;mc\_cheung@126.com>
---
+++ lib/Text/Util.pm
@@ -0,0 +1,66 @@
+package Text::Util;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = @EXPORT_OK = qw/clean_text merge_space/;
+
+sub clean_text {
+    my $str = shift;
+    return unless $str;
+    return merge_space( chompf( $str ) );
+}
+
+sub merge_space {
+    my $str = shift;
+    return '' unless $str;
+   
+    $str =~ s/\t+/ /g;
+    $str =~ s/\r\n//g;
+    $str =~ s/\s+/ /g;
+    return $str;
+}
+
+sub chompf {
+    my $str = shift;
+    return '' unless $str;
+    $str =~ s/^\s+//g;
+    $str =~ s/\s+$//g;
+    return $str;
+}
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::Util - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::Util;
+
+=head1 DESCRIPTION
+
+Text::Util is a util for clean text data, forexample merge sapce etc 
+
+=head1 LICENSE
+
+Copyright (C) mccheung.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mccheung E<lt>mc.cheung@aol.com<gt>
+
+=cut
+
